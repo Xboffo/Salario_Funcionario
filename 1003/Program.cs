bffc40afadadb177ba73ef559526a0d135248fef
Kauet Lima <kauet123@live.com>
2022-12-11 17:01:01 -0300

    Primeiro Commit

---
+++ 1003/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeroFuncionario, horasTrabalhadas;
+            double valorDaHora,salary;
+
+            numeroFuncionario = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            valorDaHora = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            salary = horasTrabalhadas * valorDaHora;
+            
+
+            Console.WriteLine("NUMBER = " + numeroFuncionario);
+            Console.WriteLine("SALARY = U$ " + salary.ToString("F2", CultureInfo.InvariantCulture)); 
+        }
+    }
+}
